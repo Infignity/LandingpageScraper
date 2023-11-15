847c927a999a4ad4a0691fbbe4b4771fae1ce1cf
wrefinity <wrefordmessi@gmail.com>
2023-11-15 12:56:04 +0000

    Refactoring

--- agent/webcrawler/items.py
+++
@@ -1,9 +0,0 @@
-"""item module definitions"""
-import scrapy
-
-
-class WebcrawlerItem(scrapy.Item):
-    """package fields"""
-    tag = scrapy.Field()
-    url = scrapy.Field()
-    text = scrapy.Field()
