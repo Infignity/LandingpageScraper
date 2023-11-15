d5292a54d9dffb4701ddcd82d7fb1dfe3b97975d
wrefinity <wrefordmessi@gmail.com>
2023-11-15 12:34:55 +0000

    Refactoring files

---
+++ src/agent/celeryconfig.py
@@ -0,0 +1,33 @@
+broker_url = 'redis://localhost:6379/0'
+result_backend = 'redis://localhost:6379/0'
+imports = ("tasks",)
+task_routes = {
+    'run_crawler': {
+        'queue': 'crawler_queue'
+    }
+}
+
+task_create_missing_queues = True
+
+# Prevent Celery sending hundreds of messages per second
+# with different diagnostic and redundant heartbeat messages.
+worker_send_task_event = True
+
+# Kill task after 2 hours
+# task_time_limit = 7200
+
+# Task will raise exception SoftTimeLimitExceeded
+# after 7200 seconds.
+task_soft_time_limit = 7200
+
+# Task messages will be acknowledged after the task has been executed,
+# not just before (the default behavior).
+task_acks_late = True
+
+# A worker takes 10 tasks from queue at a time
+# and will increase the performance.
+worker_prefetch_multiplier = 10
+
+task_serializer = "json"
+result_serializer = "json"
+accept_content = ["json"]
---
+++ src/agent/webcrawler/spiders/crawling_spider.py
@@ -0,0 +1,53 @@
+"""crawler spider"""
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from bs4 import BeautifulSoup
+import pandas as pd
+from webcrawler.items import WebcrawlerItem
+
+
+class CrawlingWeb(scrapy.Spider):
+    """web spiders for company website scrapping"""
+    name = "webcrawler"
+
+    def __init__(self, url=None, tags=None, *args, **kwargs):
+        """constructor class"""
+        super(CrawlingWeb, self).__init__(*args, **kwargs)
+        self.start_urls = url
+        self.tags = tags
+        self.data = []
+
+    def start_requests(self):
+        yield scrapy.Request(
+            self.start_urls,
+            meta={'playwright': True}
+        )
+  
+    def parse(self, response, tag="home", flag=True):
+        """parse func"""
+        try:
+            soup = BeautifulSoup(response.text, 'html.parser')
+        except AttributeError:
+            print('Response not text:', response.status, response.headers)
+            soup = BeautifulSoup('')
+        link_extractor = LinkExtractor(allow="about")
+        extracted_text = ' '.join(str(
+            soup.get_text().replace("\n", " ")).split())
+        dt = {
+            "url": response.url,
+            "tag": tag,
+            "text": extracted_text
+        }
+      
+        item = WebcrawlerItem()
+        item["tag"] = tag
+        item["url"] = response.url
+        item["text"] = extracted_text
+        yield item
+        print(dt)
+        self.data.append(dt)
+        for link in link_extractor.extract_links(response):
+            yield response.follow(
+                url=link.url,
+                cb_kwargs=dict(tag="about", flag=False)
+            )
